99574a0fea07f939a971b8bc169a21b5114f066b
pattama <pat_kadae@hotmail.com>
2022-07-11 13:52:08 +0200

    Finish transfer property  function to transfer cluster property to full adata

--- src/IMCprocess/Cluster.py
+++ src/IMCprocess/Cluster.py
@@ -11,12 +11,10 @@ import seaborn as sns
 
 def plot_cumulative_var(adata, cml_var_explained):
     """
-    The function to plot the cumulative explained variance ratio 
+    The function to plot the cumulative explained variance ratio
     as a function of the number of components
-
-
     :function: plot the cumulative explained variance ratio as a function of the number of components
-    :returns: TODO
+    :returns: figure of cumulative explain variance plot
 
     """
     x = range(len(adata.uns['pca']['variance_ratio']))
@@ -25,7 +23,7 @@ def plot_cumulative_var(adata, cml_var_explained):
     plt.scatter(x, y, s=4)
     plt.xlabel('Number of principle components')
     plt.ylabel('Cumulative explained variance')
-    #plt.title('Cumulative explained variance')
+    # plt.title('Cumulative explained variance')
     plt.show()
     return(fig)
 
@@ -43,8 +41,20 @@ def runPCA(adata):
     return adata, n_pcs
 
 
-def run_phenograph(adata):
-    k = 30  # choose k
+def runPhenograph(adata, k=30):
+    """
+    Parameters:
+    ___________
+    adata = anndata object
+    k = number of nearest neighbors to use in the first step of graph constructions
+        default setting is 30
+    return:
+    _______
+    adata with PhenoGraph_clusters data
+
+
+    """
+
     sc.settings.verbose = 0
     communities, graph, Q = phenograph.cluster(
         pd.DataFrame(adata.obsm['X_pca']), k=k)  # run PhenoGraph
@@ -54,3 +64,53 @@ def run_phenograph(adata):
     adata.uns['PhenoGraph_k'] = k
 
     return adata
+
+
+def transfer_property(adata1, adata2):
+    """
+
+    To transfer property from adata1 to adata2
+    adata1 is clustered anndata with filtered markers
+    adata2 is anndata with complete markers
+    Parameters:
+    ___________
+    adata1 = anndata object with property to transfer to adata2
+    adata2 = anndata object to get property from adata1
+    return:
+    _______
+    adata2 with PhenoGraph_clusters data
+
+    """
+    adata2.obs = adata1.obs.copy()
+    adata2.uns = adata1.uns.copy()
+    adata2.obsm = adata1.obsm.copy()
+    adata2.obsp = adata1.obsp.copy()
+    # transfer PCs to adata2
+    # adata1 and adata2 has different number of vars so we save in uns
+    adata2.uns['PCs'] = adata1.varm['PCs']
+    return adata2
+
+
+def main_clustering(adata, markers):
+    """TODO: Docstring for main_clustering.
+
+    : function: to run phenograph clustering for specific markers selections
+    : returns: anndata object with PhenoGraph_clusters data
+
+    """
+    # 1. Filter ann data with markers
+    adata_markers = adata[:, markers]
+
+    # 2. run PCA
+    adata_markers, n_pcs = runPCA(adata_markers)
+
+    # 3. run Phenograph
+    adata_markers = run_phenograph(adata_markers)
+
+    # 4.Computing the neighborhood graph
+    sc.pp.neighbors(adata_markers, n_pcs=n_pcs)
+
+    # 5. Embedding the neighborhood graph with UMAP
+    sc.tl.umap(adata_markers)
+
+    # 6. add new property from clustering into full markers exprs table of anndata
