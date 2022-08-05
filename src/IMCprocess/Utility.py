15217f1c3ef14953f364160cff6bb33e213acea7
pattama <pat_kadae@hotmail.com>
2022-08-05 14:37:57 +0200

    add plot heatmap in Utility.py

--- src/IMCprocess/Utility.py
+++ src/IMCprocess/Utility.py
@@ -1,3 +1,6 @@
+import scanpy as sc
+
+
 def filter_adata(adata, obs_var, obs_value_ls):
     """
     filter anndata by list of obs value
@@ -13,3 +16,31 @@ def filter_adata(adata, obs_var, obs_value_ls):
 
     """
     pass
+
+
+def heatmap_dendrogram(adata, groupby, figsize):
+    """
+    plot heatmap with dendrogram from anndata
+    Parameters:
+    ___________
+    adata = anndata object
+    groupby = The obs group label for plotting in heatmap ex. 'PhenoGraph_clusters' (colname from anndata.obs)
+    return:
+    _______
+    list of Axes
+    Note:
+    _______
+    To save plot from this object.
+    >> plot['heatmap_ax'].figure.savefig('output_plot/Fib_level2_AB.png', dpi = 300)
+
+    """
+
+    fig = sc.pl.heatmap(adata,
+                        adata.var_names,
+                        groupby=groupby,
+                        swap_axes=True,
+                        cmap='viridis',
+                        dendrogram=True,
+                        show=False,
+                        figsize=figsize)
+    return fig
