b0cc3f337ececb6deca29313c019280183ec3479
pattama <pat_kadae@hotmail.com>
2022-08-07 20:50:56 +0200

    add plt_intenisty function in spatial_analysis.py

--- src/IMCprocess/Spatial_analysis.py
+++ src/IMCprocess/Spatial_analysis.py
@@ -2,3 +2,12 @@ import numpy as np
 import scanpy as sc
 import pandas as pd
 from matplotlib import pyplot as plt
+
+
+def plt_intensity(adata, marker, path, name):
+    fig = adata.to_df()[marker].plot.density()
+    plt.title(name + marker)
+    if path != None:
+        plt.savefig(path+'/' + marker + '_intenisty.png', dpi=300)
+    plt.show()
+    return fig
