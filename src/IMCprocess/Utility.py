369b1e3f2cdbf8d7243c0459ae67e0ad98ce0ce2
pattama <pat_kadae@hotmail.com>
2022-08-10 00:09:01 +0200

    add function to create geoJson cell mask for qupath

--- src/IMCprocess/Utility.py
+++ src/IMCprocess/Utility.py
@@ -2,6 +2,9 @@ import scanpy as sc
 import pickle
 from upsetplot import plot
 from upsetplot import UpSet
+import json
+from skimage import io
+from skimage import measure
 
 
 def read_pickle_obj(pickle_file):
@@ -77,3 +80,92 @@ def plt_upset(binary_df):
                  show_counts=True, sort_categories_by=None).plot()
 
     return plot
+
+#################
+### CELL MASK ###
+#################
+
+
+def label_class(json_all, label_index, name_class_cell, color, mask):
+    """
+    function to create geoJson object for cell mask with color label
+
+    Parameters:
+    ___________
+    json_all = json object for append new cell mask
+    label_index = cell index to create json object with color label
+    name_class_cell = name of the cell 
+    color = color to label cell
+    mask = mask file
+    return:
+    _______
+    json object
+
+    """
+    for label_i in label_index:
+        contour = measure.find_contours(mask == label_i, 0.5)
+        evalCloseness = contour[0][-1] == contour[0][0]
+        isClosed = False not in evalCloseness
+        if isClosed:
+            y, x = contour[0].T
+            coordinates_inv = np.dstack((x, y))
+            coordinates = coordinates_inv[0].tolist()
+            json_object = {'type': 'Feature',  # 'id': 'PathAnnotationObject',
+                           'id': 'PathDetectionObject',
+                           'geometry': {'type': 'Polygon', 'coordinates': [coordinates]}, 'properties': {"classification": {"name": name_class_cell, "colorRGB": color}, 'isLocked': False, 'measurements': []}}
+            json_all.append(json_object)
+        else:
+            coords = contour[0]
+            coords_closed = np.append(coords, [coords[0]], axis=0)
+            contour_closed = [coords_closed]
+            y, x = contour_closed[0].T
+            coordinates_inv = np.dstack((x, y))
+            coordinates = coordinates_inv[0].tolist()
+            json_object = {'type': 'Feature',
+                           # 'id': 'PathAnnotationObject',
+                           'id': 'PathDetectionObject',
+                           'geometry': {'type': 'Polygon', 'coordinates': [coordinates]}, 'properties': {"classification": {"name": name_class_cell, "colorRGB": color}, 'isLocked': False, 'measurements': []}}
+            json_all.append(json_object)
+
+    return(json_all)
+
+
+def export_labelmask_geoJson(mask_file, output_file, label_dict, color_label):
+    """
+    function to create geoJson cell mask with multiple cell label
+    Parameters:
+    ___________
+        mask = probability mask file from cellprofiler
+        output_file = geoJson output path file
+        label_dict = dictionary of label cell
+        color_label = list of color for cell
+            (if there are only two type of cells: positive & negative,
+            we use color [-1,-8245601 (purple)]
+
+            )
+    return:
+    _______
+    json object
+    """
+
+    # read the image as np-array
+    mask = io.imread(mask_file)
+    # regionprops_table below requires inte
+    mask = mask.astype(np.uint32)
+    # measure properties of all cells
+    results = measure.regionprops_table(mask, properties=['label', 'coords'])
+    # convert results into pandas dataframe
+    results_df = pd.DataFrame.from_dict(results)
+
+    # label_class
+    name_class_list = list(label_dict.keys())
+    #color_label = [-1, -8245601]
+    json_export = []
+    for i in range(len(name_class_list)):
+        json_export = label_class(
+            json_export, label_dict[name_class_list[i]], name_class_list[i], color_label[i], mask)
+
+    with open(output_file, 'w') as outfile:
+        json.dump(json_export, outfile)
+
+    return json_export
