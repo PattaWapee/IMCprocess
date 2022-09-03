f803a6c4df67f00fec9c97f7b1679803ca75e3d2
pattama <pat_kadae@hotmail.com>
2022-09-03 22:24:24 +0200

    add interaction mat in spatial analysis

--- src/IMCprocess/Spatial_analysis.py
+++ src/IMCprocess/Spatial_analysis.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scanpy as sc
+import squidpy as sq
 import pandas as pd
 from matplotlib import pyplot as plt
 
@@ -24,3 +25,41 @@ def plt_intensity(adata, marker, path, name):
         plt.savefig(path+'/' + marker + '_intenisty.png', dpi=300)
     plt.show()
     return fig
+
+
+def plt_spatial(adata, obs_col, output_path, name):
+    """
+    function to plot 
+    """
+    fig = sc.pl.spatial(adata, color=obs_col,
+                        title=[name+' '+obs_col],
+                        return_fig=True,
+                        #save = output_path + '/' + marker+ '_' + name +'.png',
+                        spot_size=10, show=False)
+    fig2 = fig[0].get_figure()
+    fig2.tight_layout()
+    fig2.savefig(output_path+obs_col+'_'+name+'_spatial.png', dpi=300)
+
+
+def run_spatial_nhood(adata, obs_col, output_path, name):
+
+    sq.gr.spatial_neighbors(adata)
+    sq.gr.nhood_enrichment(adata, cluster_key=obs_col)
+    plt.figure(figsize=(8, 5))
+
+    sq.pl.nhood_enrichment(adata, cluster_key=obs_col,
+                           annotate=True,
+                           save=output_path + obs_col + '_' +
+                           name + '_NH_enrich.png',
+                           dpi=300
+                           )
+
+
+def plt_interaction_mat(adata, obs_col, output_path, name):
+
+    sq.gr.interaction_matrix(adata, cluster_key=obs_col)
+    sq.pl.interaction_matrix(adata, cluster_key=obs_col,
+                             annotate=True,
+                             save=output_path + obs_col + '_' +
+                             name + '_interaction_mat.png',
+                             dpi=300)
