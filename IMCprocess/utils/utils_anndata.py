3dc16e607cb749a41948247513c16d5cb669266c
pattama <pat_kadae@hotmail.com>
2022-11-17 00:13:00 +0100

    add create_anndata in utils_anndata

---
+++ IMCprocess/utils/utils_anndata.py
@@ -0,0 +1,28 @@
+import anndata as ad
+import scanpy as sc
+
+def create_anndata(df, X, Y, img_id):
+    """
+        Description
+        -----------
+            Create anndata from pandas dataframe 
+            with meta data of X,Y and img_id
+        Parameters
+        ----------
+            df : pandas dataframe
+            X: list or pandas series for x position
+            Y: list or pandas series for y position
+            img: image id
+            
+        Returns
+        -------
+            adata : anndata
+    """
+    
+    df.index = [f"Cell_{i:d}" for i in range(1,len(df)+1)]
+    adata = sc.AnnData(df)
+    adata.obs_names = [f"Cell_{i:d}" for i in range(1,len(adata)+1)]
+    adata.var_names = df.columns
+    adata.obsm["spatial"] = np.array(list(zip(X,Y)))
+    adata.obs['img_id'] = [img_id]* len(df)
+    return(adata)
